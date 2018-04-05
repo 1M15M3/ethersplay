73802c903895853f23d1a42ec95633cb4c7f715e
Josselin <josselin@trailofbits.com>
2018-04-05 10:20:49 +0100

    Create transfer function per ins -> possibility to stub by other analysis Save the stack values in func_name.out metadata Add show stack plugin Remove color on unanalysed bb

--- ethersplay/__init__.py
+++ ethersplay/__init__.py
@@ -4,7 +4,7 @@ from binaryninja import PluginCommand
 
 from printSourceCode import function_source_code_start
 from coverage import function_coverage_start
-from stack_value_analysis import function_dynamic_jump_parameterisable_start
+from print_stack import function_printStack_start
 from evm import EVM, EVMView
 
 PluginCommand.register("EVM Source Code",
@@ -15,9 +15,10 @@ PluginCommand.register("EVM Manticore Highlight",
                        "EVM Manticore Highlight",
                        function_coverage_start)
 
-PluginCommand.register_for_function("EVM Dynamic Jumps Recovery",
-                                    "EVM Dynamic Jumps Recovery",
-                                    function_dynamic_jump_parameterisable_start)
+
+PluginCommand.register_for_function("EVM Print stack",
+                                    "Print up to 10 values of the stack",
+                                    function_printStack_start)
 
 EVM.register()
 EVMView.register()
